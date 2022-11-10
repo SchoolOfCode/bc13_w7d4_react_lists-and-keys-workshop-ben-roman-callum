36ea7e68fd2e878f78bd3091d1252adac294c5cd
bennyfreemantle <bennyfreemantle@protonmail.com>
2022-11-10 15:59:10 +0000

    Task 1 completed

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './index.css';
+import "./index.css";
 
-import samplePosts from '../../libs/data';
+import samplePosts from "../../libs/data";
 
-import Post from '../Post';
+import Post from "../Post";
 
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
-  return <main id="main"></main>;
+  return (
+    <main id="main">
+      {posts.map(({ postId }) => {
+        return <Post key={postId} />;
+      })}
+    </main>
+  );
 }
 
 export default Main;
